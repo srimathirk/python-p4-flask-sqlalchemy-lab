7031ffd4660f257463abf0ea2c3a2b9d0df3f860
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-09-01 17:10:00 -0400

    updating sqlalchemy lab

--- server/app.py
+++ server/app.py
@@ -20,8 +20,12 @@ def home():
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
     animal = Animal.query.filter(Animal.id == id).first()
-    
+    if not animal:
+        response_body = f'<h1>404 Animal not found</h1>'
+        response = make_response(response_body,200)
+        return response
     response_body = f'''
+            <ul>Id:{animal.id}</ul>
             <ul>Name:{animal.name}</ul>
             <ul>Species:{animal.species}</ul>  
             <ul>Zookeeper:{animal.zookeeper.name}</ul>
@@ -33,24 +37,36 @@ def animal_by_id(id):
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
     zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    if not zookeeper:
+        response_body = f'<h1>404 Zookeeper not found</h1>'
+        response = make_response(response_body,200)
+        return response
     
     response_body = f'''
+            <ul>Id:{zookeeper.id}</ul>
             <ul>Name:{zookeeper.name}</ul>
-            <ul>Birthday:{zookeeper.birthday}</ul>  
-            <ul>Animal: </ul>
+            <ul>Birthday:{zookeeper.birthday}</ul>
         '''
-        
+    for animal in zookeeper.animals:
+        response_body += f'<ul>Animal: {animal.name} belong to {animal.species}</ul>'    
     response = make_response(response_body,200)
     return response
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
     enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    if not enclosure:
+        response_body = f'<h1>404 Enclosure not found</h1>'
+        response = make_response(response_body,200)
+        return response
     
+    #animals_in_enclosure = enclosure.animals
     response_body = f'''
-            <ul>Environment:{Enclosure.environment}</ul>
-            <ul>Open to Visitors:{Enclosure.open_to_visitors}</ul>  
-            <ul>Animal: </ul>
-        '''
+            <ul>Id: {enclosure.id}</ul>
+            <ul>Environment: {enclosure.environment}</ul>
+            <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>  
+         '''
+    for animal in enclosure.animals:
+        response_body += f'<ul>Animal: {animal.name} belong to {animal.species}</ul>'
     response = make_response(response_body,200)
     return response
 
